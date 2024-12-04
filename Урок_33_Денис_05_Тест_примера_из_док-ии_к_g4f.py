13e6884d20ef38d8c8ee9a47e15cb820ed3f7844
Programmer <shakirov.ekb@gmail.com>
2024-12-05 01:51:16 +0500

    Тестовый пример из документации r g4f.

---
+++ Урок_33_Денис_05_Тест_примера_из_док-ии_к_g4f.py
@@ -0,0 +1,28 @@
+import g4f
+import asyncio
+
+_providers = [
+    g4f.Provider.Blackbox
+]
+
+
+async def run_provider(provider: g4f.Provider.BaseProvider):
+    try:
+        response = await g4f.ChatCompletion.create_async(
+            model=g4f.models.default,
+            messages=[{"role": "user", "content": "Что такое шкварки?"}],
+            provider=provider,
+        )
+        print(f"{provider.__name__}:", response)
+    except Exception as e:
+        print(f"{provider.__name__}:", e)
+
+
+async def run_all():
+    calls = [
+        run_provider(provider) for provider in _providers
+    ]
+    await asyncio.gather(*calls)
+
+
+asyncio.run(run_all())
